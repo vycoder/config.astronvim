1a11721b02ae0c92515fb28cc235ac9cdc848094
yev <josephharveyangeles@gmail.com>
2023-08-16 20:32:20 +0800

    v1.0.3   - disabled volar formatting, used prettierd

--- init.lua
+++ init.lua
@@ -42,6 +42,7 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         "eslint_d",
+        "volar",
       },
       timeout_ms = 2000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
